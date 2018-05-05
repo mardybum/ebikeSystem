c39caee90f0cb93b07f156300cd46d98ba333ef1
Grillmo <Grillmo@gmx.de>
2018-05-05 10:36:05 +0200

    Combined speed and amp control, better startup performance

--- applications/ebikeCustomApplication.c
+++ applications/ebikeCustomApplication.c
@@ -1,9 +1,9 @@
 /*
-	Copyright 2017 Benjamin Vedder	benjamin@vedder.se
+    Copyright 2017 Benjamin Vedder  benjamin@vedder.se
 
-	This file is part of the VESC firmware.
+    This file is part of the VESC firmware.
 
-	The VESC firmware is free software: you can redistribute it and/or modify
+    The VESC firmware is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
@@ -31,7 +31,7 @@
 //#define mode2
 
 //HIGHLY experimental, sets engine to 2.0A in case of too high poti
-#define debug
+//#define debug
 
 static THD_FUNCTION(example_thread, arg);
 static THD_WORKING_AREA(example_thread_wa, 2048); // 2kb stack for this thread
@@ -69,17 +69,11 @@ static const float noRotatingTreshold = 3000.0;
 //timer in milli seconds for not pedaling
 static const float noPedalingTreshold = 8000.0;
 
-static const float setPointCurrentStartupLower = 15.0;
-static const float setPointCurrentStartupUpper = 10.0;
-static const float startUpRoutineVelocityTrigger = 15.0;
+static const float setPointCurrentStartup = 13.0;
 
-<<<<<<< HEAD
-//Wert scheisse?
-=======
 
 // standard current
->>>>>>> 7c3c5d721076612df226589898cf2e0bbd36b9d6
-static const float lowerCurrent = 8.0;
+static const float lowerCurrent = 7.0;
 
 //Number of magnets that have to be triggered at the crank to set isPedaling true
 static const int tresholdMagnet = 1;
@@ -88,7 +82,7 @@ static const int tresholdMagnet = 1;
 static const float tresholdTimePedaling = 15000.0;
 
 //Min speed to start the engine
-static const float minSpeed = 6.0;
+static const float minSpeed = 15.0;
 
 //TImers for on and off time pedaling
 static systime_t firstPedalStroke;
@@ -98,7 +92,16 @@ static systime_t timeStartPedaling;
 static systime_t timeMotorRelease;
 
 //Timer for the duration of a different current that will be applied (Startup routine)
-static const float startUpRoutineEnd = 2000.0;
+static const float startUpRoutineEnd = 1000.0;
+
+//Motor Poles
+static const float poles = 8.0;
+
+//Wheel to motor circumference (see excel sheet)
+static const float wheelToMotorFactor = 14.0;
+
+//Startup routine treshold
+static const float startUpVelocityTreshold = 20.0;
 
 //timer for applying motor after it was released
 static const float motorReleaseTreshold = 1500.0;
@@ -283,9 +286,7 @@ static THD_FUNCTION(example_thread, arg) {
 
         
         //TODO ABS()?
-        if(abs(differenceLow > differenceHigh)) {
-            
-         if((differenceLow > differenceHigh) && (differenceLow - differenceHigh < 2.0)) {
+        if((differenceLow > differenceHigh) && (float)ST2MS(chVTTimeElapsedSinceX(highStart)) < 1000.0) {
             
             pedalingForward = true;
         } else {
@@ -331,17 +332,15 @@ static THD_FUNCTION(example_thread, arg) {
                     startUpRoutine = false;
                 }
                 
-                //Depend the startup current on the velocity of the bikeA
-                if(vehicleVelocity > 0.0 && vehicleVelocity < startUpRoutineVelocityTrigger) {
-                    
-                    setPointCurrent = setPointCurrentStartupLower;
-                } else if (vehicleVelocity > startUpRoutineVelocityTrigger) {
-                    
-                    setPointCurrent = setPointCurrentStartupUpper;
+                if(vehicleVelocity < startUpVelocityTreshold) {
+
+                  mc_interface_set_pid_speed(poles*wheelToMotorFactor*speedSensorWheel);
+                } else {
+
+                  setPointCurrent = setPointCurrentStartup;
+                  mc_interface_set_current(setPointCurrent);
                 }
-                
-                
-                mc_interface_set_current(setPointCurrent);
+
                 
                 //vehicleVelocity = (3.14*0.0007112 * speedSensorWheel*60.0);
                 
@@ -355,20 +354,21 @@ static THD_FUNCTION(example_thread, arg) {
             //#ifndef debug
                 
                 //At least 10 Amps and additionaly the amoount of the max motor current times poti value
-                setPointCurrent = ((float) mcconf->lo_current_motor_max_now * pot) + lowerCurrent;
+                setPointCurrent = ((float) mcconf->lo_current_motor_max_now * (1-pot)) + lowerCurrent;
                 mc_interface_set_current(setPointCurrent);
+               // mc_interface_set_pid_speed(0.0);
                 
             //#endif
                 
             #ifdef debug
                 
-                if(pot > 0.95) {
+                if((1-pot) > 0.95) {
                     
                     mc_interface_set_current(1.0);
                 } else {
                     
                     //At least 10 Amps and additionaly the amoount of the max motor current times poti value
-                    setPointCurrent = ((float) mcconf->lo_current_motor_max_now * pot) + lowerCurrent;
+                    setPointCurrent = ((float) mcconf->lo_current_motor_max_now * (1-pot)) + lowerCurrent;
                     mc_interface_set_current(setPointCurrent);
                 }
                 
@@ -413,9 +413,11 @@ static THD_FUNCTION(example_thread, arg) {
             
         }
         
-        //commands_printf("hall1: %f \n", (float)pot);
-        
-        commands_printf("Hallsensor: %d \n", hallSensorCrank);
+        //commands_printf("Poti: %f \n", (float)(pot));
+        //commands_printf("Speed:  %f \n", vehicleVelocity);
+        //commands_printf("Pedaling %d \n", isPedaling);
+       //commands_printf("Pedaling %d \n", isPedaling);
+
         
         // Run this loop at 500Hz
         chThdSleepMilliseconds(2);
